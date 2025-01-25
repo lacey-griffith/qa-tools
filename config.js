12d62f986e5d2b0e2229a1927ba2dc281c5cefff
laceyhay <l.hay@fusion92.com>
2025-01-25 16:34:17 -0600

    updated generateBtns, labelEditor, Clear, addVarInputs

---
+++ config.js
@@ -0,0 +1,211 @@
+export const brands = [
+    /* NBLY */
+    {
+    brand_handle: 'aire-serve',
+    brand: 'ASV',
+    conceptID: 3,
+    prod: 'https://www.aireserv.com/',
+    staging: 'https://dig-www-nei-asv-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.aireserv.com/jacksonville',
+    staging_local_homepage:'https://dig-www-nei-asv-stage.nblytest.com/jacksonville',
+    active_client: true
+}, {
+    brand_handle: 'five-star-painting',
+    brand: 'FSP',
+    conceptID: 4,
+    prod: 'https://www.fivestarpainting.com/',
+    staging: 'https://dig-www-nei-fsp-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.fivestarpainting.com/lagrange/',
+    staging_local_homepage:'https://dig-www-nei-fsp-stage.nblytest.com/lagrange',
+    active_client: true
+}, {
+    brand_handle: 'grounds-guys',
+    brand: 'GUY',
+    conceptID: 6,
+    prod: 'https://www.groundsguys.com/',
+    staging: 'https://dig-www-nei-guy-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.groundsguys.com/sparksnv/',
+    staging_local_homepage:'https://dig-www-nei-guy-stage.nblytest.com/sparksnv',
+    active_client: true
+}, {
+    brand_handle: 'glass-doctor',
+    brand: 'MDG',
+    conceptID: 5,
+    prod: 'https://www.glassdoctor.com/',
+    staging: 'https://develop-dwyr-mdg.pantheonsite.io/',
+    neighborly: true,
+    prod_local_homepage:'https://www.glassdoctor.com/tacoma-wa',
+    staging_local_homepage:'https://develop-dwyr-mdg.pantheonsite.io/tacoma-wa/',
+    active_client: true
+}, {
+    brand_handle: 'molly-maid',
+    brand: 'MLY',
+    conceptID: 1,
+    prod: 'https://www.mollymaid.com/',
+    staging: 'https://dig-www-nei-mly2-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.mollymaid.com/chelmsford/',
+    staging_local_homepage:'https://dig-www-nei-mly2-stage.nblytest.com/chelmsford/',
+    active_client: true
+}, {
+    brand_handle: 'mosquito-joe',
+    brand: 'MOJ',
+    conceptID: 18,
+    prod: 'https://mosquitojoe.com/',
+    staging: 'https://dig-www-nei-moj2-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://mosquitojoe.com/locations/northern-delaware/',
+    staging_local_homepage:'https://dig-www-nei-moj2-stage.nblytest.com/locations/northern-delaware/',
+    active_client: true
+},{
+    brand_handle: 'mr-appliance',
+    brand: 'MRA',
+    conceptID: 8,
+    prod: 'https://www.mrappliance.com/',
+    staging: 'https://dig-www-nei-mra-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.mrappliance.com/indianapolis/',
+    staging_local_homepage:'https://dig-www-nei-mra-stage.nblytest.com/indianapolis/',
+    active_client: true
+}, {
+    brand_handle: 'mr-electric',
+    brand: 'MRE',
+    conceptID: 9,
+    prod: 'https://mrelectric.com/',
+    staging: 'https://dig-www-nei-mre2.nblyprod.com/',
+    neighborly: true,
+    prod_local_homepage:'https://mrelectric.com/san-antonio',
+    staging_local_homepage:'https://dig-www-nei-mre2.nblyprod.com/san-antonio',
+    active_client: true
+}, {
+    brand_handle: 'mr-handyman',
+    brand: 'MRH',
+    conceptID: 2,
+    prod: 'https://www.mrhandyman.com/',
+    staging: 'https://dig-www-nei-mrh-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.mrhandyman.com/south-essex-county/',
+    staging_local_homepage:'https://dig-www-nei-mrh-stage.nblytest.com/south-essex-county/',
+    active_client: true
+}, {
+    brand_handle: 'mr-rooter',
+    brand: 'MRR',
+    conceptID: 10,
+    prod: 'https://www.mrrooter.com/',
+    staging: 'https://dig-www-nei-mrr-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.mrrooter.com/austin',
+    staging_local_homepage:'https://dig-www-nei-mrr-stage.nblytest.com/austin',
+    active_client: true
+},{
+    brand_handle: 'window-genie',
+    brand: 'WDG',
+    conceptID: 16,
+    prod: 'https://www.windowgenie.com/',
+    staging: 'https://dig-www-nei-wdg2-stage.nblytest.com/',
+    neighborly: true,
+    prod_local_homepage:'https://www.windowgenie.com/west-austin',
+    staging_local_homepage:'https://dig-www-nei-wdg2-stage.nblytest.com/west-austin',
+    active_client: true
+},
+/* NON NBLY */
+{
+    brand_handle: 'adm',
+    brand: 'ADM',
+    conceptID: 0,
+    prod: 'https://www.adm.com/',
+    staging: '',
+    neighborly: false,
+    active_client: true
+},{
+    brand_handle: 'sun-home-saunas',
+    brand: 'Sun Home Saunas',
+    conceptID: 0,
+    prod: 'https://www.sunhomesaunas.com/',
+    staging: '',
+    neighborly: false,
+    active_client: true
+},{
+    brand_handle: 'yoga-works',
+    brand: 'Yoga Works',
+    conceptID: 0,
+    prod: 'https://yogaworks.com/',
+    staging: '',
+    neighborly: false,
+    active_client: true
+},{
+    brand_handle: 'lights-fest',
+    brand: 'Lights Festival',
+    conceptID: 0,
+    prod: 'https://www.thelightsfest.com/',
+    staging: '',
+    neighborly: false,
+    active_client: true
+},{
+    brand_handle: 'lancer-skincare',
+    brand: 'Lancer',
+    conceptID: 0,
+    prod: 'https://www.lancerskincare.com/',
+    staging: '',
+    neighborly: false,
+    active_client: true
+},{
+    brand_handle: 'lexie-hearing',
+    brand: 'Lexie Hearing',
+    conceptID: 0,
+    prod: 'https://www.lexiehearing.com/us',
+    staging: '',
+    neighborly: false,
+    active_client: false
+},{
+    brand_handle: 'go-hearing',
+    brand: 'Go Hearing',
+    conceptID: 0,
+    prod: 'https://www.gohearing.com/',
+    staging: '',
+    neighborly: false,
+    active_client: false
+},{
+    brand_handle: '1up-nutrition',
+    brand: '1UP',
+    conceptID: 0,
+    prod: 'https://www.1upnutrition.com/',
+    staging: '',
+    neighborly: false,
+    active_client: false
+},{
+    brand_handle: 'emler-swim-school',
+    brand: 'Emler',
+    conceptID: 0,
+    prod: 'https://www.emlerswimschool.com/',
+    staging: '',
+    neighborly: false,
+    active_client: false
+},{
+    brand_handle: 'fresh-pressed-olive-oil',
+    brand: 'FPOO',
+    conceptID: 0,
+    prod: 'https://www.freshpressedoliveoil.com/',
+    staging: '',
+    neighborly: false,
+    active_client: true
+},{
+    brand_handle: 'moon-juice',
+    brand: 'Moon Juice',
+    conceptID: 0,
+    prod: 'https://www.moonjuice.com/',
+    staging: '',
+    neighborly: false,
+    active_client: false
+},{
+    brand_handle: 'spot-loan',
+    brand: 'Spot Loan',
+    conceptID: 0,
+    prod: 'https://www.spotloan.com/',
+    staging: '',
+    neighborly: false,
+    active_client: true
+}];
---
+++ script_urlGenerator.js
@@ -0,0 +1,256 @@
+import { brands } from './config.js';
+
+let nblyForm = `<div class='nbly-form'>
+                    <div>
+                        <div class="form-group checkbox-container">
+                            <input type="checkbox" id="local-pages" name="local-pages">
+                            <label for="local-pages">Local Pages</label>
+                        </div>
+                        <div class="form-group checkbox-container">
+                            <input type="checkbox" id="national-pages" name="national-pages">
+                            <label for="national-pages">National Pages</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="prod-url">Prod Natl URL:</label>
+                        <input type="text" id="prod-url" name="prod-url" placeholder="Enter Prod URL">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="staging-url">Staging Natl URL:</label>
+                        <input type="text" id="staging-url" name="staging-url" placeholder="Enter Staging URL">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="prod-local-url">Prod Local URL:</label>
+                        <input type="text" id="prod-local-url" name="local-url" placeholder="Enter Prod Local URL">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="staging-local-url">Staging Local URL:</label>
+                        <input type="text" id="staging-local-url" name="staging-local-url" placeholder="Enter Staging Local URL">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="qa-param">QA Parameter:</label>
+                        <input type="text" id="qa-param" name="qa-param" placeholder="Enter QA Parameter">
+                    </div>
+
+                    <div id="variation-group-container">
+                        <div class="variation-group">
+                            <label contendeditable="true for="variation-control">Control:</label>
+                            <input type="text" id="variation-control" name="variation-live-qa[]"
+                                placeholder="Enter Preview Link">
+                        </div>
+
+                        <div class="variation-group">
+                            <label contendeditable="true for="variation-1">V1:</label>
+                            <input type="text" id="variation-1" name="variation-live-qa[]"
+                                placeholder="Enter Preview Link">
+                        </div>
+                        <button class="btn add-var" type="button">+</button>
+                    </div>
+                    <button class="btn" id="clear-button">Clear</button>
+                    <button class="btn" type="button" onclick="generateUrls()">Generate</button>
+                </div>`;
+
+let regForm = `
+        <div class='reg-form'>
+                    <div class="form-group">
+                        <label for="url">URL:</label>
+                        <input type="text" id="url" name="url" placeholder="Enter URL">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="staging-url">Staging URL:</label>
+                        <input type="text" id="staging-url" name="staging-url" placeholder="Enter Staging URL">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="qa-param">QA Parameter:</label>
+                        <input type="text" id="qa-param" name="qa-param" placeholder="Enter QA Parameter">
+                    </div>
+
+                    <div id="variation-group-container">
+                        <div class="variation-group">
+                            <label contendeditable="true for="variation-control">Control:</label>
+                            <input type="text" id="variation-control" name="variation-live-qa[]"
+                                placeholder="Enter Preview Link">
+                        </div>
+
+                        <div class="variation-group">
+                            <label contendeditable="true for="variation-1">V1:</label>
+                            <input type="text" id="variation-1" name="variation-live-qa[]"
+                                placeholder="Enter Preview Link">
+                        </div>
+                        <button class="btn add-var" type="button">+</button>
+                    </div>
+                    <button class="btn" id="clear-button">Clear</button>
+                    <button class="btn" type="button" onclick="generateUrls()">Generate</button>
+                </div>`;
+
+(function () {
+    function addSteps() {
+        //target parent container
+        let container = $('section#url-generator');
+        container.addClass('step-container no-form');
+
+        //add brand btn contaiiner
+        container.append(`<h4>QA URL Generator</h4>
+            <div id='form' class='form-option'>
+            <div class='tool-inner'>
+                <div id='stepOne' class=''>
+                    <h2>Choose Client:</h2>
+                    <div class='brand-btn-container initial'></div>
+                </div>
+                <div class='url-generator-inner tool-body transparent-background'></div>
+            </div>
+        </div>
+        <div id="output" class="output" ></div>`);
+
+        //build button for each active brand
+        brands.forEach(function (item, i) {
+            //skip inactive clients
+            if (!item.active_client) {
+                return;
+            }
+            let btn = `<button data-handle='${item.brand_handle}' data-neighborly=${item.neighborly}>${item.brand}</button>`;
+            $('#stepOne .brand-btn-container').append(btn);
+        });
+
+        //send user to step 2 on button click
+        $('#stepOne button').on('click', function () {
+            $('.brand-btn-container').removeClass('initial');
+            let handle = $(this).attr('data-handle');
+
+            //remove active class from other buttons
+            if (!$(this).hasClass('active')) {
+                $('#stepOne button.active').removeClass('active');
+                $(this).addClass('active');
+            }
+
+            //build forms
+            buildForm(handle);
+        });
+    }
+
+    function buildForm(handle) {
+        let activeBrand = brands.find(brand => brand.brand_handle === handle);
+        let markUp = ``;
+
+        //if brand.neighborly is true
+        if (activeBrand.neighborly) {
+            markUp = nblyForm;
+            //if brand.neighborly is false
+        } else if (!activeBrand.neighborly) {
+            markUp = regForm;
+        }
+
+        $('section#url-generator').removeClass('no-form').addClass('form-present');
+
+        //add respective form to page and show it
+        $('#form .tool-body').html(markUp);
+        $('#form .tool-body').removeClass('transparent-background');
+
+        //Set values based on brand info
+        $('#form #prod-url').val(activeBrand.prod)
+        $('#form #staging-url').val(activeBrand.staging)
+
+        //include neighborly local pages
+        if (activeBrand.neighborly) {
+            $('#form #prod-local-url').val(activeBrand.prod_local_homepage)
+            $('#form #staging-local-url').val(activeBrand.staging_local_homepage)
+        }
+
+        //add event listener for adding a variaton btn (+)
+        $('button.btn.add-var').on('click', function () {
+            addVariationInput();
+        });
+
+        //make labels editable
+        $('section#url-generator #variation-group-container .variation-group').each((i, eL) => {
+            let label = $(eL).find('label');
+            labelEditor(label);
+        });
+
+        //add event listener to clear form
+        $('#clear-button').on('click', function () {
+            Clear();
+        });
+    }
+
+    //Add more variations
+    let varCount = 2;
+    function addVariationInput() {
+        varCount++;
+        //target container
+        let varContainer = $('#variation-group-container');
+
+        let newVar = `<div class="variation-group">
+            <label contendeditable="true" for="variation-${varCount - 1}">V${varCount - 1}:</label>
+            <input type="text" id="variation-${varCount - 1}" name="variation-live-qa[]" placeholder="Enter Preview Link">
+        </div>`;
+
+        //add new label to page and make it editable
+        varContainer.append(newVar);
+        let label = $(`.variation-group [for="variation-${varCount - 1}"]`);
+        labelEditor(label);
+    }
+
+    // Variation Label editing
+    const labelEditor = (label) => {
+        label.attr('contenteditable', 'true');
+
+        label.on('click', function () {
+            let text = $(this).text();
+            $(this).replaceWith(`<input class="editing-label" autofocus/>`);
+            $('input.editing-label').val(text).focus();
+            labelEditorEvents(text);
+        });
+    };
+
+    function labelEditorEvents(OgText) {
+        $('input.editing-label').on('blur keydown', function (e) {
+            //only trigger for enter key
+            if (e.type === 'keydown' && e.keyCode !== 13) {
+                return;
+            }
+
+            e.preventDefault();
+            let text = $(this).val();
+            if (text === '') {
+                text = OgText;
+            }
+
+            let labelFor = $(this).next('input').attr('id');
+            $(this).replaceWith(`<label for=${labelFor}>${text}</label>`);
+            labelEditor($(`label[for=${labelFor}]`));
+        });
+    }
+
+    //function generateUrls(){}
+
+    function Clear(){
+        //remove additional variations that may have been added
+        const variations = $('#url-generator input[id^="variation-"]');
+        variations.each((i, input) => {
+            //remove any that are not variation-control or variation-1
+            if(input.id !== 'variation-control' && input.id !== 'variation-1'){
+                $(input).parent().remove();
+            } else {
+                //determine if its not the control, must be v1, apply text accordingly
+                const labelText = input.id === 'variation-control' ? 'Control:' : 'V1:';
+                $(input).val('');
+                //console.log(labelText)
+                console.log($(input).parent().find('label'))
+                $(input).parent().find('label').text(labelText);
+            }
+        })
+    }
+
+
+    document.addEventListener('DOMContentLoaded', function (e) {
+        addSteps();
+    });
+})();
